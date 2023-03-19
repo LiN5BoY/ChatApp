46c974a9a96793d64862cb268b1145cf5668f463
LiN5BoY <2311549594@qq.com>
2023-03-19 20:33:06 +0800

    0319

--- app/src/main/java/com/example/chatapp/activities/SignInActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignInActivity.java
@@ -2,15 +2,28 @@ package com.example.chatapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        //对于一个没有被载入或者想要动态载入的界面，都需要使用LayoutInflater.inflate()来载入；
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        //根视图获取
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        binding.textCreateNewAccount.setOnClickListener(v -> startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
+++ app/src/main/java/com/example/chatapp/activities/SignUpActivity.java
@@ -5,12 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivitySignInBinding;
+import com.example.chatapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        //对于一个没有被载入或者想要动态载入的界面，都需要使用LayoutInflater.inflate()来载入；
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        //根视图获取
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+
+    private void setListeners(){
+        binding.textSignIn.setOnClickListener(v -> onBackPressed());
     }
 }
